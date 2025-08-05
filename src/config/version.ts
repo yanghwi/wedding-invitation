45f13b444cdbe2694d8865a32bc005974bb01e28
yanghwi <531cake@gmail.com>
2025-08-05 21:24:04 +0900

    GitHub Actions 배포 워크플로우 단순화 및 안정성 개선

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,9 +1,9 @@
 // 이 파일은 빌드 시마다 자동으로 업데이트됩니다
-// Build Time: 2025-08-05T12:20:22.294Z
+// Build Time: 2025-08-05T12:23:19.022Z
 export const APP_VERSION = {
-  version: '1754396422294',
-  buildTime: '2025-08-05T12:20:22.294Z',
-  hash: 'xd4jbwskfr8'
+  version: '1754396599022',
+  buildTime: '2025-08-05T12:23:19.022Z',
+  hash: 'nkbw5mxzl8n'
 };
 
 export default APP_VERSION;
